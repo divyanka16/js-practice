370de9235070a717ae6345b9f2834e6feaf391a7
divyanka16 <anothersideofdivya@gmail.com>
2025-10-05 14:01:59 +0000

    topic-1 added

--- 01_basics/index.js
+++ 01_basics/index.js
@@ -1,1 +1,18 @@
 console.log("Hello from 01_basics!");
+
+
+// Topic - 1, ---- VARIABLES ----
+
+// variables is just like storing a data in a container, that's called variables.
+// three types of variables:- const, let, var.
+// var:- It's a old way and didn't advised to use more.
+// const:- It's for values that don't change or reassignable.
+// let:- modern way, can be changed but not reassignable.
+
+var namee = "john doe";
+let email = "john@google.com";
+const password = "1234";
+
+gender = "male"; // you can store values like this too but it's not a good practice or suggested.
+
+console.table([namee, email, password, gender]);
